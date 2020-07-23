5bd39a0f07c787fd7e55f5174149d1f845f48ab3
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2020-07-23 12:04:01 +0300

    Fix links

--- src/general-items-price-change/Application.svelte
+++ src/general-items-price-change/Application.svelte
@@ -1,1 +1,11 @@
-<p>Общее изменение цен на все тур</p>
+<div class="row">
+  <div class="span12 hspan">
+    <h4>Общее изменение цен на все туры</h4>
+  </div>
+</div>
+
+<div class="row">
+  <span class="span12">
+    <p>Здесь изменения</p>
+  </span>
+</div>
