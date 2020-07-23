95a6e2e5d12f5742e5550c55c93bf1eb813c0fef
Andrey Pavlenko <31999255+andrey-pavlenko@users.noreply.github.com>
2020-07-23 10:19:10 +0300

    Add prettier

--- src/application-first/Application.svelte
+++ src/application-first/Application.svelte
@@ -1,19 +1,24 @@
-<script lang="ts">
-  export let name: string;
-  export let surName: string = 'No surName defined';
-
-  function getName(): string {
-    return 'Test';
-  }
-</script>
-
 <style>
   h2 {
     color: blue;
   }
 </style>
 
+<script lang="ts">
+  export let name: string;
+  export let surName: string = "No surName defined";
+
+  function someTest(input: string): string {
+    if (input === "1") {
+      return "one";
+    }
+    return "many";
+  }
+</script>
+
 <main>
   <h1>Hello {name}!</h1>
   <h2>Hello {surName}!</h2>
+  <p>{someTest('1')}</p>
+  <p>{someTest('2')}</p>
 </main>
